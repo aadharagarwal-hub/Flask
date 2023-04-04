0dd705215b189c77827e6eeefe56da5ccf6e57fc
aadharagarwal-hub <aadhar-agarwal@uiowa.edu>
2023-04-04 15:27:12 -0500

    added flash

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "alsknq3rAg$GernaeasSEF^woei4r098HRFYUKioq73498"
+
 
 friends_dict = [
     {"name": "Test", "flavor": "swirl", "read": "yes", "activities": "reading"}
@@ -18,24 +20,36 @@ def not_found_error(error):
 def internal_error(error):
     return render_template("500.html"), 500
 
-# Route for the homepage
-@app.route("/")
-def index():
-    return render_template("index.html", pageTitle="Web form template", friends=friends_dict)
 
-# Route for adding a new friend
-@app.route("/add", methods=["GET", "POST"])
+###### Routes ######
+
+# Home page
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template(
+        "index.html", pageTitle="Web form template", friends=friends_dict
+    )
+
+
+# /add route for form submission
+@app.route("/add", methods=["POST"])
 def add():
+    print("inside add function")
     if request.method == "POST":
-        # Process the form data and add a new friend to the list
+
         form = request.form
 
         fname = form["fname"]
         flavor = form["flavor"]
         read = form["read"]
-        activities = form.getlist("activities")
+        activities = form.getlist("activities")  # this is a PYthon list
 
-        activities_string = ", ".join(activities)
+        print(fname)
+        print(flavor)
+        print(read)
+        print(activities)
+
+        activities_string = ", ".join(activities)  # make the Python list into a string
 
         friend_dict = {
             "name": fname,
@@ -44,12 +58,20 @@ def add():
             "activities": activities_string,
         }
 
-        friends_dict.append(friend_dict)
+        print(friend_dict)
+        friends_dict.append(
+            friend_dict
+        )  # append this dictionary entry to the larger friends dictionary
+        print(friends_dict)
 
+        flash(
+            "The friend ;" + fname + " has been added to the database.",
+            "success",
+        )
         return redirect(url_for("index"))
     else:
-        # Render the form template for GET requests
-        return render_template("add.html", pageTitle="Add a new friend")
+        return redirect(url_for("index"))
+
 
 # Route for the about page
 @app.route("/about")
